ad765c3131a74e1c28df1dd453fd5cafc7e02f7e
Mikhail Andreev (aka adw0rd) <x11org@gmail.com>
2012-02-29 21:10:39 +0400

    Rewrited the code for correctly jquery plugin and added function_test.html

--- jquery.autofocus.js
+++ jquery.autofocus.js
@@ -3,30 +3,35 @@
  * Set the focus to the desired form element when text is entered without a focus
  * Copyright (c) 2012 Mikhail Andreev (adw0rd.ru)
  */
-jQuery.autofocus = function(input, options) {
-    var keypress_words = [];
-    var keypress_limit = options['keypress_limit'] ? options['keypress_limit'] : 0;
-    var has_focus = false;
-
-    input.focus(function(){
-		has_focus = true;
-	}).blur(function() {
-		has_focus = false;
-	});
-
-    $(document).keypress(function(button){
-        //console.log(button.charCode, button.ctrlKey, button.altKey)
-        if (!has_focus) {
-            if (button.charCode && !button.ctrlKey && !button.altKey) {
-                var char = String.fromCharCode(button.charCode);
-                keypress_words.push(char);
-            }
-            if (keypress_words.length >= keypress_limit) {
-                input.val(keypress_words.join('')).focus();
-            }
-        } else {
-			// flush words
-            keypress_words = [];
+(function($) {
+    $.fn.autofocus = function(options) {
+        var defaults = {
+            keypress_limit: 0
         }
-    });
-}
+        var options = $.extend(defaults, options);
+        var keypress_words = [];
+        var has_focus = false;
+        var input = $(this);
+
+        input.focus(function(){
+            has_focus = true;
+        }).blur(function() {
+            has_focus = false;
+        });
+
+        $(document).keypress(function(button){
+            if (!has_focus) {
+                if (button.charCode && !button.ctrlKey && !button.altKey) {
+                    var ch = String.fromCharCode(button.charCode);
+                    keypress_words.push(ch);
+                }
+                if (keypress_words.length >= options.keypress_limit) {
+                    input.focus().val(input.val() + keypress_words.join(''));
+                }
+            } else {
+                // flush words
+                keypress_words = [];
+            }
+        });
+    }
+}(jQuery));
